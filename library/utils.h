43f069d7b792f1d4d4642251e05d5d7baf4fd39d
julian <julian.desvignes@telecom-sudparis.eu>
2015-11-25 00:02:12 +0100

    Correction utils.h

--- library/utils.h
+++ library/utils.h
@@ -5,6 +5,14 @@
 #define MAX(x,y) (((x)>(y))?(x):(y))
 #define ABS(x) (((x) > 0) ? (x) : -(x))
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int modulo(int nombre, int modulo);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
